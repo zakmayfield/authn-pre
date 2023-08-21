2296d8cc696dca7e47da0d77d9442e4e76dc2c43
Zak Mayfield <zakmayfield@gmail.com>
2023-08-21 14:47:24 -0400

    chore: added public route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/foo'],
+  publicRoutes: ['/', '/api/create-account/:id'],
 });
 
 export const config = {
