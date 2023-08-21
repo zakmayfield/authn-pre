f51e8572194650c91d0bc66aa571a8da58cf13fe
Zak Mayfield <zakmayfield@gmail.com>
2023-08-21 14:51:37 -0400

    feat: Welcome page account initialized

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,5 +1,22 @@
-// i need to access the userId from clerk.auth() and run a prisma query to create a new user
+import { auth } from '@clerk/nextjs';
 
-export default function Welcome() {
+async function createAccount(userId: string | null) {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/create-account/${userId}`
+    );
+    return response.json();
+  } catch (error) {
+    console.log('page:error', error);
+  }
+}
+
+export default async function Welcome() {
+  const { userId } = auth();
+  console.log('page:userId', userId);
+  const data = await createAccount(userId);
+
+  console.log('page:data', data);
+
   return <div>Welcome</div>;
 }
